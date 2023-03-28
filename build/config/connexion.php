13ec75b56e7357f2e3150797f7811cefcbc4f2df
Nabil BELLILA <bellilanabil8@gmail.com>
2023-03-28 10:18:08 +0200

    main fix

--- build/config/connexion.php
+++ build/config/connexion.php
@@ -12,7 +12,7 @@ try {
 
         $con = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
         return $con;
-         ;
+        ;
     } catch (Exception $e) {
         die('Erreur : '.$e->getMessage());
     }
