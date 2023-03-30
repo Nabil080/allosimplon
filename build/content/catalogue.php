e037b8a9f288eb55a2c6855938ac356f8ee151e7
Nabil BELLILA <bellilanabil8@gmail.com>
2023-03-30 11:23:31 +0200

    tri order by + filtre where note =

--- build/content/catalogue.php
+++ build/content/catalogue.php
@@ -17,11 +17,11 @@ require_once '../config/functions.php';
 <!-- PAGINATION -->
 <?php
 // Page active
-if (!isset ($_GET['page']) ) {$page_number = 1;
+if (!isset ($_GET['page']) ) {$page_number = 1; $_GET['page']= 1;
 } else {$page_number = $_GET['page'];}
 $pagination_number = $page_number;
 // Limite de lignes par page
-$limit = 4;
+$limit = 16;
           // echo 'limite par page :'; var_dump($limit);
 $initial_page = ($page_number-1)*$limit;
           // echo' initial page: ';var_dump($initial_page);
@@ -31,10 +31,112 @@ $film_count= $film_count_request->rowCount();
           // echo' Nombre de films: ';var_dump($film_count);
 $page_count = ceil($film_count/$limit);
           // echo' Nombre de pages nécessaires: ';var_dump($page_count);
-$rqs=$con->prepare("SELECT * FROM film LIMIT " . $initial_page . ',' . $limit);$rqs->execute();
 
 
+$filters=[];
+var_dump($filters);
+
+var_dump($_GET);
+if(isset($_GET['note'])){$filters['note']=$_GET['note'];}
+
+if(isset($_GET['sort'])){$filters['sort']=$_GET['sort'];}
+
+$get_genre_array=[];
+var_dump($get_genre_array);
+if(isset($_GET['genre'])){
+  $get_genre_array=$_GET['genre'];
+  var_dump($get_genre_array);
+  $filters['genre'] = implode(", ",$get_genre_array);
+  $get_film_genre_request=$con->prepare("SELECT ID_film FROM film_genre WHERE ID_genre IN (" . $filters['genre'] . ")");$get_film_genre_request->execute();
+  while($film_genre_array = $get_film_genre_request->fetch()){
+        $get_film_array[]=$film_genre_array['ID_film'];
+        echo($film_genre_array['ID_film']);
+      }
+  var_dump($get_film_array);
+  $filters['film'] = implode(", ",$get_film_array);
+  var_dump($filters);
+}
+
+if(isset($filters['film'])){
+   if(!isset($clause)){
+      $clause = " " . "WHERE ID_film IN (" . $filters['film'] .")" ;
+   }else{
+      $clause .= " " . " AND ID_film IN (" . $filters['film'] . ")" ;
+   }
+}
+
+if(isset($filters['note'])){
+   if(!isset($clause)){
+      $clause = " " . "WHERE film_grade >=" . $filters['note'] ;
+   }else{
+      $clause .= " AND film_grade >=" . $filters['note'] ;
+    }
+}
+
+if(isset($filters['sort'])){
+  if($filters['sort']=="a-z"){$order_name="film_name ASC";};
+  if($filters['sort']=="z-a"){$order_name="film_name DESC";};
+  if($filters['sort']=="rand"){$order_name="rand()";};
+  if($filters['sort']=="asc"){$order_name="ID_film ASC";};
+  if($filters['sort']=="desc"){$order_name="ID_film DESC";};
+  if($filters['sort']=="grade"){$order_name="film_grade DESC";};
+
+  $order = " ORDER BY " . $order_name ;
+}
+
+if(empty($filters)){
+  $film_request =$con->prepare("SELECT * FROM film LIMIT $initial_page,$limit");
+}
+if(isset($clause)){
+  if(isset($order)){
+    $film_request = $con->prepare("SELECT * FROM film $clause $order LIMIT $initial_page,$limit");
+  }else{
+    $film_request = $con->prepare("SELECT * FROM film $clause LIMIT $inital_page,$limit");
+  }
+}
+
+if(isset($order) && !isset($clause)){
+  $film_request = $con->prepare("SELECT * FROM film $order LIMIT $initial_page,$limit");
+}
+
+$film_request->execute();
+var_dump($film_request);
+// var_dump($_SESSION['filters']);
+
+if(isset($_GET['page'])){
+  if(!isset($_SESSION['filters'])){
+      $_SESSION['filters'] =  "page=".$_GET['page'];echo 'orv';
+  }else{
+      $_SESSION['filters'] = "&page=" .  $_GET['page'];echo'slt';
+  }
+}
+
+if(isset($_GET['note'])){
+  if(!isset($_SESSION['filters'])){
+      $_SESSION['filters'] =  "note=".$_GET['note'];
+  }else{
+      $_SESSION['filters'] .= "&note=" .  $_GET['note'];
+  }
+}
+
+if(isset($_GET['genre'])){
+  if(!isset($_SESSION['filters'])){
+      $_SESSION['filters'] = "genre=".$_GET['genre'];
+  }else{
+      $_SESSION['filters'] .= "&genre=" . implode(", ",$_GET['genre']);
+}
+}
+
+if(isset($_GET['sort'])){
+  if(!isset($_SESSION['filters'])){
+      $_SESSION['filters'] = "sort=".$_GET['sort'];
+  }else{
+      $_SESSION['filters'] .= "&sort=" . $_GET['sort'];
+}
+}
+var_dump($_SESSION['filters']);
 ?>
+
 
 <!-- SECTION CATALOGUE -->
 
@@ -90,7 +192,7 @@ $rqs=$con->prepare("SELECT * FROM film LIMIT " . $initial_page . ',' . $limit);$
     <!-- CATALOGUE -->
         <div class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:-grid-cols-5 [&_img]:w-full [&_img]:h-full object-cover ">
             <?php
-                while($film=$rqs->fetch()){
+                while($film=$film_request->fetch()){
                     $photo=$film['film_photo'];
                     $name=$film['film_name'];
                     $ID=$film['ID_film'];
--- build/include/general/filtre.php
+++ build/include/general/filtre.php
@@ -1,44 +1,42 @@
+<?php
+
+  if(empty($_SESSION['filters'])){
+    $url = "/portfolio/allosimplon/build/content/catalogue.php?";
+  }else{
+    $url = "/portfolio/allosimplon/build/content/catalogue.php?" . $_SESSION['filters']  . "&";
+  }
+//   var_dump($url);
+?>
+
+
 <!-- FILTRES -->
 <div class="grow w-full max-w-[230px] hidden md:block">
         <div class="font-bold text-3xl w-fit notewe">Note IMDb</div>
-        <div class=" my-8 grade text-main-light z-50 text-2xl">
-            <i class="fa-solid fa-star cursor-pointer"></i>
-            <i class="fa-solid fa-star cursor-pointer"></i>
-            <i class="fa-solid fa-star cursor-pointer"></i>
-            <i class="fa-regular fa-star cursor-pointer relative group/star"><i class="fa-solid fa-star cursor-pointer absolute top-0 left-0 hidden group-hover/star:block"></i></i>
-            <i class="fa-regular fa-star cursor-pointer relative group/star"><i class="fa-solid fa-star cursor-pointer absolute top-0 left-0 hidden group-hover/star:block"></i></i>
+        <div class="flex gap-x-2 font-bold w-fit my-8 grade text-main-light z-50 text-2xl">
+            <a href="<?=$url?>note=2"><button name="one_star"> <input type="text" class="hidden" value="one_star"><i class="fa-regular fa-star cursor-pointer"></i></button></a>
+            <a href="<?=$url?>note=4"><button name="two_star"> <input type="text" class="hidden" value="two_star"><i class="fa-regular fa-star cursor-pointer"></i></button></a>
+            <a href="<?=$url?>note=6"><button name="three_star"> <input type="text" class="hidden" value="three_star"><i class="fa-regular fa-star cursor-pointer"></i></button></a>
+            <a href="<?=$url?>note=8"><button name="four_star"> <input type="text" class="hidden" value="four_star"><i class="fa-regular fa-star cursor-pointer"></i></button></a>
+            <a href="<?=$url?>note=10"><button name="five_star"> <input type="text" class="hidden" value="five_star"><i class="fa-regular fa-star cursor-pointer"></i></button></a>
         </div>
         <div class="font-bold text-3xl w-fit notewe">Trier par</div>
         <div class="my-8 flex flex-wrap gap-1">
-            <button class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>A-Z</button>
-            <button class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>Z-A</button>
-            <button class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>Aléatoire</button>
-            <button class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>Ascendant</button>
-            <button class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>Descendant</button>
-            <button class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>Notes</button>
-            <button class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>Vues</button>
-            <button class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>Publication</button>
+            <a href="<?=$url?>sort=a-z"><button class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>A-Z</button></a>
+            <a href="<?=$url?>sort=z-a"><button class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>Z-A</button></a>
+            <a href="<?=$url?>sort=rand"><button class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>Aléatoire</button></a>
+            <a href="<?=$url?>sort=asc"><button class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>Ascendant</button></a>
+            <a href="<?=$url?>sort=desc"><button class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>Descendant</button></a>
+            <a href="<?=$url?>sort=grade"><button class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>Notes</button></a>
+            <a href="<?=$url?>sort=a-z"><button class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>Vues</button></a>
+            <a href="<?=$url?>sort=a-z"><button class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>Publication</button></a>
         </div>
         <div class="font-bold text-3xl w-fit notewe">Genres</div>
         <div class="my-8 flex flex-wrap gap-1">
-            <button id="slt" class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>Action</button>
-            <button class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>Amateur</button>
-            <button class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>Animation</button>
-            <button class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>Aventure</button>
-            <button class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>Catastrophe</button>
-            <button class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>Comédie</button>
-            <button class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>Documentaire</button>
-            <button class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>Drame</button>
-            <button class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>Fantastique</button>
-            <button class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>Guerre</button>
-            <button class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>Histoire</button>
-            <button class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>Médiéval</button>
-            <button class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>Musical</button>
-            <button class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>Opéra</button>
-            <button class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>Crime</button>
-            <button class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>Sciences-fiction</button>
-            <button class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>Sketchs</button>
-            <button class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>Sport</button>
-            <button class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i>Western</button>
+            <?php $genre_request=$con->prepare("SELECT * FROM genre");$genre_request->execute();
+            while($genre=$genre_request->fetch()){ ?>
+            <a href="<?=$url?>genre=<?=$genre['ID_genre']?>">
+                <button id="<?=$genre['ID_genre']?>" class="border-gray-700 p-2 hover:border-main-light hover:bg-main-hover border-2 rounded-md active:text-black" ><i class="fa-solid fa-plus"></i><?=$genre['genre_name']?></button>
+            </a>
+            <?php } ?>
         </div>
     </div>
